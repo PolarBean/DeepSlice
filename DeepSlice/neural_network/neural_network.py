1647fe7f4ce27ab254706d77b692fb47cb0da5c0
Samuel Le Meur-Diebolt <samuel@diebolt.io>
2026-04-29 11:00:42 +0000

    add pyproject packaging and Python 3.13 image support

--- DeepSlice/neural_network/neural_network.py
+++ DeepSlice/neural_network/neural_network.py
@@ -10,9 +10,8 @@ import numpy as np
 import os
 from skimage.color import rgb2gray
 import warnings
-import imghdr
-import struct
 import h5py
+from PIL import Image
 
 
 def gray_scale(img: np.ndarray) -> np.ndarray:
@@ -236,36 +235,6 @@ def predictions_util(
 
 
 def get_image_size(fname):
-    # https://stackoverflow.com/questions/8032642/how-to-obtain-image-size-using-standard-python-class-without-using-external-lib
-    """Determine the image type of fhandle and return its size.
-    from draco"""
-    with open(fname, "rb") as fhandle:
-        head = fhandle.read(24)
-        if len(head) != 24:
-            raise Exception("Invalid header")
-
-        ext = imghdr.what(fname)
-        if imghdr.what(fname) == "png":
-            check = struct.unpack(">i", head[4:8])[0]
-            if check != 0x0D0A1A0A:
-                raise Exception("png checksum failed")
-            width, height = struct.unpack(">ii", head[16:24])
-        elif imghdr.what(fname) == "gif":
-            width, height = struct.unpack("<HH", head[6:10])
-        elif imghdr.what(fname) == "jpeg":
-            fhandle.seek(0)  # Read 0xff next
-            size = 2
-            ftype = 0
-            while not 0xC0 <= ftype <= 0xCF:
-                fhandle.seek(size, 1)
-                byte = fhandle.read(1)
-                while ord(byte) == 0xFF:
-                    byte = fhandle.read(1)
-                ftype = ord(byte)
-                size = struct.unpack(">H", fhandle.read(2))[0] - 2
-            # We are at a SOFn block
-            fhandle.seek(1, 1)  # Skip `precision' byte.
-            height, width = struct.unpack(">HH", fhandle.read(4))
-        else:
-            raise Exception(f"Invalid filetype: {head}")
-        return width, height
+    with Image.open(fname) as image:
+        width, height = image.size
+    return width, height
